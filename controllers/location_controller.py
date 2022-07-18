4019b8be61691b5aca25d6e7bf82290d69f8be50
Samuel <samuelsf@hotmail.co.uk>
2022-07-18 10:13:20 +0100

    created location routes, added 4x html files for locaiton

--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -2,26 +2,60 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.location import Location
 import repositories.location_repository as location_repository
+import repositories.hobby_repository as hobby_repository
 
 location_blueprint = Blueprint("locations", __name__)
 
 # INDEX
-# GET '/locations'
+@location_blueprint.route('/locations')
+def locations():
+    locations = location_repository.select_all()
+    return render_template('locations/index.html', all_locations = locations)
 
 # NEW
-# GET '/location/new'
+@location_blueprint.route('/location/new', methods=['GET'])
+def new_location():
+    hobbies = hobby_repository.select_all()
+    return render_template('locations/new.html', all_hobbies = hobbies)
 
 # CREATE
-# POST '/locations'
+@location_blueprint.route('/locations', methods=['POST'])
+def create_location():
+    name                 = request.form['location_name']
+    description          = request.form['location_description']
+    distance_to_location = request.form['location_distance_to_location']
+    reminder             = request.form['location_reminder']
+    location = Location(name, description, distance_to_location, reminder)
+    location_repository.save(location)
+    return redirect('/locations')
 
 # SHOW
-# GET '/locations/<id>'
+@location_blueprint.route('/locations/<id>', methods=['GET'])
+def show_location(id):
+    location = location_repository.select(id)
+    return render_template('locations/show.html', location = location)
 
 # EDIT
-# GET '/locations/<id>/edit'
+@location_blueprint.route('/locations/<id>/edit', methods=['GET'])
+def edit_location(id):
+    location = location_repository.select(id)
+    hobby = hobby_repository.select_all()
+    return render_template('hobbies/edit.html', location = location, all_hobbies = hobby)
 
 # UPDATE
-# PUT (POST) '/locations/<id>'
+@location_blueprint.route('/locations/<id>', methods=['POST'])
+def update_location(id):
+    name                 = request.form['location_name']
+    description          = request.form['location_description']
+    distance_to_location = request.form['location_distance_to_location']
+    reminder             = request.form['location_reminder']
+    location = Location(name, description, distance_to_location, reminder, id)
+    location_repository.save(location)
+    return redirect('/locations')
 
 # DELETE
-# DELTE (POST) '/locations/<id>
+# DELTE (POST) '/hobbies/<id>
+@location_blueprint.route('/locations/<id>/delete', methods=['POST'])
+def delete_location(id):
+    location_repository.delete(id)
+    return redirect('/locations')
